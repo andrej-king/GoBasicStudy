ceb1e9cd7a7b5f1004ff9619e87f5e7670d33d40
Andrew <graduadu@gmail.com>
2021-10-03 20:17:28 +0300

    Added funcs with params

--- lesson_2/app.go
+++ lesson_2/app.go
@@ -1,10 +1,22 @@
 package main
 
-import "fmt"
-
-const name string = "John"
+//const name string = "John"
 
 func main() {
-	surname := "Doe"
-	fmt.Println(fmt.Sprintf("%s %s", name, surname))
+	//surname := "Doe"
+	//fmt.Println(fmt.Sprintf("%s %s", name, surname))
+}
+
+// basic func with return multi param types
+func test() (string, string) {
+	a := "hello"
+	b := "world"
+
+	return a, b
+}
+
+// basic func with return one param types
+func test1() string {
+	a := "hello"
+	return a
 }
