0714ce2f629d7bfa994ba1267f68e9396997f643
Andrey <graduadu@gmail.com>
2021-10-04 12:33:02 +0300

    Added initialized parameters. Added loops (for, while). Added if, switch conditions. Added deffer (Delayed operation).

--- lesson_2/app.go
+++ lesson_2/app.go
@@ -1,10 +1,20 @@
 package main
 
+import "fmt"
+
 //const name string = "John"
 
 func main() {
 	//surname := "Doe"
 	//fmt.Println(fmt.Sprintf("%s %s", name, surname))
+
+	// Will be executed at the end of the script LIFO (Last In, First Out)
+	defer fmt.Println("Delayed operation 1")
+	defer fmt.Println("Delayed operation 2")
+	defer fmt.Println("Delayed operation 3")
+
+	//fmt.Println(forLoop(10))
+	conditionsTest()
 }
 
 // struct instead class
@@ -37,4 +47,63 @@ func test() (string, string) {
 func test1() string {
 	a := "hello"
 	return a
+}
+
+// Out parameters have already been initialized
+func namedReturnParams() (a, b string) {
+	a = "Hello "
+	b = "world"
+	return a, b // can be "return" but not recommend.
+}
+
+// simple "for" loop
+func forLoop(maxVal int) int {
+	sum := 0
+	for i := 0; i < maxVal; i++ {
+		sum += i
+	}
+
+	return sum
+}
+
+// analog "while" loop
+func whileLoop(maxVal int) {
+	sum := 0
+	for sum < maxVal {
+		sum += 1
+	}
+
+	fmt.Println(sum)
+}
+
+// endless loop
+func endlessLoop() {
+	sum := 0
+	for {
+		sum += 1
+		fmt.Println(sum)
+	}
+}
+
+// simple if and if with initialize, switch
+func conditionsTest() {
+	desiredResult := 45
+	for i := 0; i <= 10; i++ {
+		if j := forLoop(i); j == desiredResult {
+			fmt.Println(desiredResult)
+		}
+
+		if i == 3 {
+			fmt.Println(i)
+		}
+	}
+
+	for i := 0; i <= 10; i++ {
+		switch j := forLoop(i); j {
+		case desiredResult:
+			fmt.Println(desiredResult)
+		default:
+			fmt.Println(i)
+		}
+	}
 }
