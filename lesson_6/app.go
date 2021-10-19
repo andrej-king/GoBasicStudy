8063a92d4438739f05cc9f3eebddb93e4704489e
Andrey <graduadu@gmail.com>
2021-10-19 16:29:29 +0300

    Added interface as type

--- lesson_6/app.go
+++ lesson_6/app.go
@@ -13,16 +13,49 @@ func main() {
 	//a = os
 	//fmt.Println(a.sum()) // 7
 	//
-	var i someInterface = otherStructure{5, 10}
-	fmt.Println(i.sum()) // 15
+	//var i someInterface = otherStructure{5, 10}
+	//fmt.Println(i.sum()) // 15
 
-	fmt.Printf("%v, %T\n", i, i) // {5 10}, main.otherStructure
+	//fmt.Printf("%v, %T\n", i, i) // {5 10}, main.otherStructure
 
-	var os1 *someStructure
-	var emptyInterface someInterface
-	fmt.Printf("%v, %T\n", emptyInterface, emptyInterface) // <nil>, <nil>
-	emptyInterface = os1
-	fmt.Printf("%v, %T\n", emptyInterface, emptyInterface) // <nil>, *main.someStructure
+	//var os1 *someStructure
+	//var emptyInterface someInterface
+	//fmt.Printf("%v, %T\n", emptyInterface, emptyInterface) // <nil>, <nil>
+	//emptyInterface = os1
+	//fmt.Printf("%v, %T\n", emptyInterface, emptyInterface) // <nil>, *main.someStructure
+
+	// Casting to type
+	//var a interface{} = "hello"
+	//s := a.(string) // Casting to string type
+	//fmt.Println(s)
+
+	// Check if it is possible to cast to this type
+	//s, ok := a.(string)
+	//fmt.Println(s, ok) // hello true
+
+	//s, ok := a.(float32)
+	//fmt.Println(s, ok) // 0 false
+	//if ok {
+	//	//write logic for use var "s"
+	//}
+
+	//f := a.(float32)
+	//fmt.Println(f) // panic: interface conversion: interface {} is string, not float32
+
+	// check var type
+	var b interface{} = true
+	switch b.(type) {
+	case int:
+		fmt.Println("a is int")
+	case string:
+		fmt.Println("a is string")
+	default:
+		fmt.Printf("unknown type %T\n", b) // unknown type bool
+	}
+
+	// structure for JSON
+	//var json map[string]interface{} // string key, value any type
+
 }
 
 type someInterface interface {
