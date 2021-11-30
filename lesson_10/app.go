31bce64b406507c7b6c7f2d2a2a921c90eda3aba
Andrej Pedchenko <graduadu@gmail.com>
2021-11-30 20:49:56 +0200

    Used mutex for lock and unlock critical section.

---
+++ lesson_10/app.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Counter struct {
+	mu sync.Mutex
+	c  map[string]int
+}
+
+func (c *Counter) Inc(key string) {
+	c.mu.Lock()   // lock to use
+	c.c[key]++    // critical section (in one time can use only one goroutine.
+	c.mu.Unlock() // unlock to use
+}
+
+func (c *Counter) Value(key string) int {
+	return c.c[key]
+}
+
+// Multithreading. Synchronization primitives
+func main() {
+	key := "test"
+	c := Counter{c: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c.Inc(key)
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Println(c.Value(key))
+}
